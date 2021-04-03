7fc2b5aead6bf9fe667b463d484d5d6950ec0e17
chowanij <jan.chowaniec@outlook.com>
2021-04-03 22:59:44 +0200

    error handling

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
@@ -22,9 +23,30 @@ func addValues(x, y int) int {
 	return x + y
 }
 
+// Divide - /didivide request handler
+func Divide(rw http.ResponseWriter, r *http.Request) {
+	var x, y float32
+	x, y = 10.5, -0.008
+	value, err := divide(x, y)
+	if err != nil {
+		fmt.Fprintf(rw, "Cannot divide by 0")
+		return
+	}
+	fmt.Fprintf(rw, fmt.Sprintf("%f divided by %f is %f", x, y, value))
+}
+
+func divide(x, y float32) (float32, error) {
+	if y == 0 {
+		return 0, errors.New("devided by 0")
+	}
+	result := x / y
+	return result, nil
+}
+
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
+	http.HandleFunc("/divide", Divide)
 
 	fmt.Println(fmt.Sprintf("Server listening on port %s....", portNum))
 	_ = http.ListenAndServe(portNum, nil)
